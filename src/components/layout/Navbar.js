83159be71f48ed9f19f4b124308366de1d561dfd
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-18 15:46:52 +0530

    Styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import '../layout/style.css'
 
 
 const Navbar = () => {
     return(
         <nav className="nav-wrapper black darken-3">
             <div className="container">
-                <Link to='/' className="brand-logo">Manage Projects</Link>
+                <Link to='/' className="logo">Manage Projects</Link>
                 <SignedInLinks/>
                 <SignedOutLinks/>
             </div>
