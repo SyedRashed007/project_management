8bd98e1178e623ee06354b2c198ba541bc462378
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-18 20:58:38 +0530

    Title

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return(
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <h4 className="card-title">Project Title</h4>
+                <h4 className="card-title">{project.title}</h4>
                 <p>Posted by Syed Rashed</p>
                 <p className="grey-text">18th May, 2021</p>
             </div>
