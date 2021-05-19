897e911045abeaf5d849775e2ea10bc0b00c6650
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-20 03:25:23 +0530

    Details added

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,24 +1,55 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import {Redirect} from 'react-router-dom'
+ 
 
-function ProjectDetails(props) {
+const  ProjectDetails = (props) => {
 
+    // const id = props.match.params.id;
     // console.log(props)
-    const id = props.match.params.id;
 
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim accusamus consectetur quam doloribus, earum quaerat aut repellat, pariatur excepturi beatae quibusdam impedit maiores corrupti illum fuga repellendus corporis odio! Officiis.</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Syed Rashed</div>
-                    <div>18th May 2021</div>
+    const {auth, project} = props;
+    if (!auth.uid) return <Redirect to='/signin'/>
+    if (project) {
+        return(
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{project.title}</span>
+                        <p>{project.content}</p>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+                        <div>18th May 2021</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return(
+            <div className="container center">
+                <p>Click on Manage Projects...</p>
+            </div>
+        )
+    }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    // console.log(state);
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+    return {
+        project: project,
+        auth: state.firebase.auth
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projcts', orderBy: [["title", "desc"]]}
+    ])
+)(ProjectDetails)
