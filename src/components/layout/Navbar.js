b5fafb6ebf983b398705bf215462e9431031655b
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-20 03:23:52 +0530

    Nav change fixed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,18 +3,33 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import '../layout/style.css'
+import { connect } from 'react-redux'
+import {isLoaded} from 'react-redux-firebase'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    const { auth } = props;
+    // console.log(auth)
+
+   const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>
+
     return(
         <nav className="nav-wrapper black darken-3">
             <div className="container">
                 <Link to='/' className="logo left"><b>Manage Projects</b></Link>
-                <SignedInLinks/>
-                <SignedOutLinks/>
+                { isLoaded(auth)&&links }
             </div>
         </nav>
     )
 }
 
-export default Navbar
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return{
+        auth: state.firebase.auth
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
