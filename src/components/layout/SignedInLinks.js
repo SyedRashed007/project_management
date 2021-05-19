cf63cea91f3126d4daab93389aa1b012d2ad4316
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-20 03:24:23 +0530

    auth added

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../layout/style.css'
+import {connect} from 'react-redux'
+import {signOut} from '../../store/actions/AuthActions'
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
     return(
-        <ul className="right">
-            <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
-            <li><NavLink to='/' className="btn btn-floating grey lighten-1">SR</NavLink></li>
-        </ul>
+        <div>
+            <ul className="right">
+                <li><NavLink to='/create'>New Project</NavLink></li>
+                <li><button className='button' onClick={props.signOut}>Log Out</button></li>
+                <li><NavLink to='/' className="btn btn-floating grey lighten-1">SR</NavLink></li>
+            </ul>
+        </div>
     )
 }
 
-export default SignedInLinks
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
