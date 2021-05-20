5c953e445f0fff58210d4c80f4e15f9137a84538
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-20 17:00:51 +0530

    Initials added

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -10,7 +10,7 @@ const SignedInLinks = (props) => {
             <ul className="right">
                 <li><NavLink to='/create'>New Project</NavLink></li>
                 <li><button className='button' onClick={props.signOut}>Log Out</button></li>
-                <li><NavLink to='/' className="btn btn-floating grey lighten-1">SR</NavLink></li>
+                <li><NavLink to='/' className="btn btn-floating grey lighten-1">{props.profile.initials}</NavLink></li>
             </ul>
         </div>
     )
