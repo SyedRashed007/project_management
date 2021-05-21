de2899ca976d845febf633e61d421945bcf5091b
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-21 20:21:37 +0530

    Added todo

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList'
-import Notifications from './Notifications'
+import Todo from './Todo'
 import { connect } from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
@@ -20,7 +20,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                       <Notifications/>
+                       <Todo/>
                     </div>
                 </div>
             </div>
